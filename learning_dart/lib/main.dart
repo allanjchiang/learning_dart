0785ed4b231943481642f91d2b6585aefd505b1a
Allan Chiang <allanchiang24@gmail.com>
2023-10-08 15:17:59 +1300

    Delete everything inside test function

--- learning_dart/lib/main.dart
+++ learning_dart/lib/main.dart
@@ -4,12 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
-void test(String? firstName, String? middleName, String? lastName) {
-  String? name = firstName;
-  name ??= middleName;
-  name ??= lastName;
-  print(name);
-}
+void test() {}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,7 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    test('Bar', null, 'Baz');
+    test();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
