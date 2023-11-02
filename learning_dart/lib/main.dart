b690e5c8a881115ea1b42f271c6856534dfd86b2
Allan Chiang <allanchiang24@gmail.com>
2023-11-02 15:54:23 +1300

    Remove old code

--- learning_dart/lib/main.dart
+++ learning_dart/lib/main.dart
@@ -4,18 +4,7 @@ void main() {
   runApp(const MyApp());
 }
 
-Stream<String> getName() {
-  return Stream.periodic(const Duration(seconds: 1), (value) {
-    return 'Foo';
-  });
-}
-
-void test() async {
-  await for (final value in getName()) {
-    print(value);
-  }
-  print('Stream finished working');
-}
+void test() async {}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
